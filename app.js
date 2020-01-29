ddeb30795f43bbcd3fe4a48a5f613e97b8effbcf
Бернова Анна <bast33@yandex.ru>
2020-01-29 20:36:21 +0300

    add function of selecting bikes

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const bike = (name, model,owner,year,phone,image)=>({name, model, owner, year, phone, image})
 
 const bikes = [
-    bike('Cannondale', 'Habit', 'Ann', 2018, '+7 925 787 27 55','images/cannondaleBad'),
-    bike('Specialized', 'Stumpjumper', 'Igor', 2019, '+7 925 787 299 55','images/StumpiEvo')
+    bike('Cannondale', 'Habit', 'Ann', 2018, '+7 925 787 27 55','images/cannondaleBad.jpg'),
+    bike('Specialized', 'Stumpjumper', 'Igor', 2019, '+7 925 787 299 55','images/StumpiEvo.jpg')
 ]
 
 new Vue ({
     el: '#app',
     data:{
-        bikes:bikes
+        bikes:bikes,
+        bike:bikes[0],
+        selectedBikeIndex:0,
+        phoneVisibility:false,
+    },
+    methods:{
+        selectBike: function(index){
+            this.bike = bikes[index]
+            this.selectedBikeIndex =index
+        }
     }
 })
