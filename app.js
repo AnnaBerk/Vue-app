efdca2e38910ad574dddce9f35ba3ed0e104b4b8
Бернова Анна <bast33@yandex.ru>
2020-01-30 23:05:40 +0300

    add modal window

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const bike = (name, model,owner,year,phone,image)=>({name, model, owner, year, phone, image})
+const log = (text, type, date = new Date()) =>({text, type, date})
 
 const bikes = [
-    bike('Cannondale', 'Habit', 'Ann', 2018, '+7 925 787 27 55','images/cannondaleBad.jpg'),
+    bike('Cannondale', 'Habit', 'Gabi', 2018, '+7 925 787 27 55','images/cannondaleBad.jpg'),
     bike('Specialized', 'Stumpjumper', 'Igor', 2019, '+7 925 787 299 55','images/StumpiEvo.jpg')
 ]
 
@@ -10,15 +11,31 @@ new Vue ({
     data:{
         bikes:bikes,
         bike:bikes[0],
+        logs:[],
         selectedBikeIndex:0,
         phoneVisibility:false,
-        search:''
+        search:'',
+        modalVisibility:false,
     },
     methods:{
-        selectBike: function(index){
+        selectBike(index){
             this.bike = bikes[index]
             this.selectedBikeIndex =index
+        },
+        newOrder(){
+            this.modalVisibility = false
+            this.logs.push(
+                log(`Succes order:${this.bike.name} - ${this.bike.model} `,'ok')
+            )
+
+        },
+        cancelOrder(){
+            this.modalVisibility = false
+            this.logs.push(
+                log(`Cancelled order:${this.bike.name} - ${this.bike.model} `,'cnl')
+            )
         }
+
     },
     computed:{
         phoneBtnChangeText(){
@@ -29,5 +46,12 @@ new Vue ({
                 return bike.name.indexOf(this.search)> -1 || bike.model.indexOf(this.search)>-1
             })
         }
+    },
+
+    filters: {
+        date(value){
+            return value.toLocaleString()
+        }
+
     }
 })
